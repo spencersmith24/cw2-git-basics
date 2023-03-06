83130217b08764f8b1b72b9c458e3d255ad58795
melody <melodylynnnh@gmail.com>
2023-03-06 10:04:03 -0500

    add exception handling & update `chdir` call

--- players/players_viewer.py
+++ players/players_viewer.py
@@ -1,11 +1,15 @@
 if __name__ == '__main__':
-    from os import chdir, getcwd
+    from os import chdir
+    from os.path import dirname, realpath
     from xml.etree.ElementTree import parse
-    if getcwd()[-1] == 's': chdir("players")
+    chdir(dirname(realpath(__file__)))
     players = parse('players.xml').getroot()
     print("The players of the CyberWater 2 Git basics challenge are…\n")
     for player in players.findall('player'):
         print(player.attrib["name"].title()+"! (https://github.com/%s/)"%player.text.replace('\n','').rstrip())
-        for thing in player.findall('favorite'):
-            print('\t• Their favorite %s is %s!'%(thing.attrib["type"], thing.text))
+        if favorites := player.findall('favorite'):
+            for thing in favorites:
+                try: print('\t• Their favorite %s is %s!'%(thing.attrib["type"], thing.text))
+                except KeyError: raise KeyError("No “type” attribute was provided! (Add it by adding “type=\"something\"” to your item's tag!)")
+        else: raise KeyError("Either no “favorite” keys were added, or they weren't added as child keys to “player!”")
     print("\nThanks for playing!")
